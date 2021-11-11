ca35d2cb0e6f454aaed75c0468d812f07e8a0b0d
Allie Soliz <solizale@gmail.com>
2021-11-11 18:55:29 -0500

    updated db name

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Cluster0", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
 	useFindAndModify: false,
 });
 
